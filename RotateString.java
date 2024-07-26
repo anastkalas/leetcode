44a636d93926b9dd0d34d8bc27df38c0cfe7d78c
anastkalas <157131789+anastkalas@users.noreply.github.com>
2024-07-26 20:04:18 +0300

    Create RotateString.java

---
+++ RotateString.java
@@ -0,0 +1,9 @@
+class Solution {
+  public boolean rotateString(String s, String goal){
+    if(s.length() != goal.length()){
+      return false;
+    }
+    String rotated = s + s;
+    return rotated.contains(goal);
+  }
+}
